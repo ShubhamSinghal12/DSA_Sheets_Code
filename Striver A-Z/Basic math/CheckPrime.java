803e289bba2d3cc25acce85f304b67e5c7d2958c
Shubham Singhal <shubhamsinghal12@gmail.com>
2024-09-20 11:22:10 +0530

    Check Prime

---
+++ Striver A-Z/Basic math/CheckPrime.java
@@ -0,0 +1,12 @@
+public class Solution {
+	public static String isPrime(int num) {
+		//Your code goes here
+		if(num == 1)return "NO";
+		for(int i = 2; i*i <= num;i++)
+		{
+			if(num%i == 0)
+				return "NO";
+		}
+		return "YES";
+	}
+}
